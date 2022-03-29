b458e8f603a9de86a134d76d8ec449029ca04b6f
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-03-29 22:54:37 +0000

    correct

--- bigofunction2.py
+++ bigofunction2.py
@@ -1,15 +1,15 @@
 def anotherfunchallenge(inpt):
-	a = 5
-	b = 10
-	c = 50
+	a = 5 #O(1)
+	b = 10 #O(1)
+	c = 50 #O(1)
 	for i in range(len(inpt)):
-		x = i + 1
-		y = i + 2
-		z = i + 3
+		x = i + 1 #O(n)
+		y = i + 2 #O(n)
+		z = i + 3 #O(n)
 	for j in range(len(inpt)):
-		p = j * 2
-		q = j * 2
+		p = j * 2 #O(n)
+		q = j * 2 #O(n)
 
-	whoami="I don't know"
+	whoami="I don't know" #O(1)
 		
-# O(4n+7), O(n)
+# O(5n+4), O(n)
