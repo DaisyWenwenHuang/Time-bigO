9a873562ea2d03b706c3289e7f9217a0b033a1a5
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-03-29 22:19:01 +0000

    add a function

--- lineartime.py
+++ lineartime.py
@@ -21,3 +21,20 @@ def whatis_fav_color(array):
 whatis_fav_color(fav_color)
 whatis_fav_color(color)
 whatis_fav_color(large_array)
+
+
+def funchallenge(inpt):
+	temp = 10 #O(1)
+	temp = temp+50 # O(1)
+	a = 0 #O(1)
+	for i in range(len(inpt)): #O(n)
+		if i:  #n*O(1)
+			a += 1 #n*O(1)
+ 	return print(a) # O(1)
+
+funchallenge(fav_color)
+funchallenge(color)
+funchallenge(large_array)
+
+# total runing time of the funchallenge function :
+#O(1+1+1+n+n*1+n*1+1)=O(3*n+4) becomes O(n)
