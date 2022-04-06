f14fb7ef374bc910ee019af0299a94822f98bc33
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-06 23:11:04 +0000

    update

---
+++ fofthemostfrequentele.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        # sort the array first
+        nums.sort()
+        
+        res=0
+        l,r=0,0
+        total =0
+        
+        while r < len(nums):
+            total += nums[r]
+            
+            while nums[r]*(r-l+1) > total +k:
+                total -= nums[l]
+                l += 1
+            
+            res=max(res,(r-l+1))
+            r += 1
+        return res
+                
