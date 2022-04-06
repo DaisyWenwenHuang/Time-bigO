e62293e626cd98c6e7fd812a24e0bf95dab20343
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-06 14:30:30 +0000

    update

--- longestrepeatingcharreplacement.py
+++ longestrepeatingcharreplacement.py
@@ -1,1 +1,19 @@
 # https://leetcode.com/problems/longest-repeating-character-replacement/
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l=0
+        r=0
+        W={}
+        l_max = 0
+
+        for r in range(len(s)):
+            W[s[r]] = 1 + W.get(s[r],0)
+            
+            if (r - l + 1) - max(W.values()) <= k:
+                l_max = max(l_max,(r - l + 1))
+            else:
+                W[s[l]] -= 1
+                l += 1
+            
+        return l_max
+        
