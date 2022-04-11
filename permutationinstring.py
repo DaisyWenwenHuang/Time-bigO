9d3da5fce1af44a1b2de14bcbefdb1ff839926a0
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-11 17:25:39 +0000

    update

---
+++ permutationinstring.py
@@ -0,0 +1,3 @@
+# https://leetcode.com/problems/permutation-in-string/
+# premutations of a string are different orderings of a string
+# for example "abc", its permutation can be  ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
