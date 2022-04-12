7e570bc5084e9a2548b9b70a11df980efc49f9ed
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-12 04:25:21 +0000

    update

--- minsizesubarraysum.py
+++ minsizesubarraysum.py
@@ -16,3 +16,19 @@ class Solution:
             return 0
 
 # better vision
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l, total = 0, 0
+        minlen = float('infinity')
+        
+        for r in range(len(nums)):
+            total += nums[r]
+            while total >= target:
+                minlen = min(minlen, (r - l + 1))
+                total -= nums[l]
+                l += 1
+        if minlen != float('infinity'):
+            return minlen
+        else:
+            return 0
+        
