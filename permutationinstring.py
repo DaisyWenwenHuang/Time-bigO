983f36816cacaa003876ef0141f999244f51f791
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-04-12 03:46:33 +0000

    update

--- permutationinstring.py
+++ permutationinstring.py
@@ -33,4 +33,47 @@ class Solution:
 # better solution
 # can iterate the two lists, s1 and s2,
 # make two count hashmaps to compair how many matehces are there
-# 
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+      # counttable for s1 and sliding window to track targeted letters
+        # using list 
+        countS = [0] * 26
+        countW = [0] * 26
+        
+        m = len(s1)
+        if m > len(s2):
+            return False
+        
+        for i in range(m):
+            # ord() to get the ascii integer of the letter
+            # subtract the ascii integer of a which makes the index of letter
+            # a 0, and b 1, etc.
+            countS[ord(s1[i]) - ord('a')] += 1
+            countW[ord(s2[i]) - ord('a')] += 1
+        
+        match = 0
+        for i in range(26):
+            if countS[i] == countW[i]:
+                match += 1
+        
+        l = 0
+        for r in range(m, len(s2)):
+            if match == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            countW[index] += 1
+            if countW[index] == countS[index]:
+                match += 1
+            elif countW[index] == countS[index] + 1:
+                match -= 1
+                
+            index = ord(s2[l]) - ord('a')
+            countW[index] -= 1
+            if countW[index] == countS[index]:
+                match += 1
+            elif countW[index] == countS[index] - 1:
+                match -=1
+            l += 1
+        return match == 26
+                
